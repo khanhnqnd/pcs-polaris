851a12298319d3edb87a8f444e50c5232aefac6b
Khanh Nguyen <khanhnq.nd@gmail.com>
2023-06-27 08:58:24 +0700

    update cli

---
+++ cli.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const inquirer = require('inquirer');
+inquirer
+    .print([
+        {
+            type: "question",
+            name: "generate",
+            message: "Generate ReactJS app for PCS admin",
+            default: "yes"
+        }
+    ]).then((answers) => {
+    try {
+        if (answers.pokemonName == "yes") {
+            console.log("yes");
+        } else {
+            console.log("no");
+        }
+    } catch (err) {
+        console.log("Not found, try again");
+    }
+})
