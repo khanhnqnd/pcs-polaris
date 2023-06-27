17c1d7b2d4852fccf19a4ced605b7be542168154
Khanh Nguyen <khanhnq.nd@gmail.com>
2023-06-27 11:16:25 +0700

    update lib

--- src/cli.ts
+++ src/cli.ts
@@ -1,23 +1,41 @@
 #!/usr/bin/env node
 
-import inquirer from 'inquirer'
-inquirer
-  .prompt([
-    {
-      type: 'question',
-      name: 'generate',
-      message: 'Generate ReactJS app for PCS admin',
-      default: 'yes',
-    },
-  ])
-  .then((answers) => {
-    try {
-      if (answers.pokemonName == 'yes') {
-        console.log('yes')
-      } else {
-        console.log('no')
-      }
-    } catch (err) {
-      console.log('Not found, try again')
-    }
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const util = require('util')
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const { spawn } = require('child_process')
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const fse = require('fs-extra')
+
+function mySpawn(command: string, args: string[], options: any, cb: any) {
+  const child = spawn(command, args, options)
+  child.on('close', (exitCode: any) => {
+    cb(null, exitCode)
   })
+  return child
+}
+
+const mySpawnPromisified = util.promisify(mySpawn)
+
+mySpawnPromisified('yarn', ['add', 'react@18.2.0'], { stdio: 'inherit' }).then(function (exitCode: any) {
+  console.log('Installed react@18.2.0')
+  console.log(`Exited with: ${exitCode}`)
+})
+
+mySpawnPromisified('yarn', ['add', 'react-dom@18.2.0'], { stdio: 'inherit' }).then(function (exitCode: any) {
+  console.log('Install react-dom@18.2.0')
+  console.log(`Exited with: ${exitCode}`)
+})
+
+function copyReact() {
+  const srcDir = `./node_modules/pcs-polaris/templates/v1`
+  const destDir = `./`
+  // To copy a folder or file, select overwrite accordingly
+  try {
+    fse.copySync(srcDir, destDir, { overwrite: false })
+    console.log('success!')
+  } catch (err) {
+    console.error(err)
+  }
+}
+copyReact()
